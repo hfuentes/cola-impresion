7eac5e846d0b045f4d07037bd86152dd4630cf0f
Héctor Fuentes <hectorfuentes@infocred.cl>
2019-06-25 12:50:45 -0400

    server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,8 @@
-console.log('HOLA HOLA PIRINOLA!');
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hola Mundo');
+});
+app.listen(3099, () => {
+	console.log('Servidor iniciado en puerto 3099');
+});
